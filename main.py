d6477a90e132932944eb4f78303e0238a15b7bcb
itsthetimmy <t.scheuermann2000@gmail.com>
2020-03-14 11:58:11 +0100

    GetSaveDir

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk, filedialog
+from tkinter import ttk, filedialog, messagebox
 from tkinter.scrolledtext import ScrolledText
 import os, sys
 from pathlib import Path
@@ -12,13 +12,22 @@ class App():
         self.CreateUI(master)
     
     def CreateUI(self, master):
-        self.downloadB = ttk.Button(master, text="Nummerieren", command=lambda: self.num(master))
+        self.selectB = ttk.Button(master, text="...", command=lambda: commingsoon())
+        self.selectB.config(width=3)
+        self.selectB.pack(padx=50,pady=50)
+        self.downloadB = ttk.Button(master, text="Ausführen", command=lambda: self.num(master))
         self.downloadB.config(width=20)
-        self.downloadB.pack(pady=2)
+        self.downloadB.pack(padx=50,pady=50)
 
     def num(self,master):
-        print("test")
+        
 
+    def GetSaveDir(self):
+        self.osname = sys.platform
+        self.location = filedialog.askdirectory()
+        self.location=(str(Path(self.location)))
+        self.getLocE.delete(0, "end")
+        self.getLocE.insert(0, self.location)
 
 def Start():
     root = tk.Tk()
@@ -26,6 +35,10 @@ def Start():
     root.resizable(0,0)
     App(root)
     root.mainloop()
+
+def commingsoon():
+    tk.messagebox.showinfo("","Wurde noch nicht implementiert!")
+
 
 if __name__ == "__main__":
     Start()
